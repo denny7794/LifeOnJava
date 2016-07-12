7ed111ee1a318655974e07f39005d1e6a24e789c
denny7794 <denny7794@gmail.com>
2016-07-12 10:40:25 +0400

    add empty window

--- src/runner/Runner.java
+++ src/runner/Runner.java
@@ -1,7 +1,20 @@
 package runner;
 
+import sun.applet.Main;
+import x.gui.MainWindow;
+
 /**
  * Created by Denis on 05.07.2016.
  */
 public class Runner {
+
+    public static void main(String[] args) {
+        try {
+            MainWindow mainWindow = new MainWindow();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
---
+++ src/x/gui/MainWindow.java
@@ -0,0 +1,18 @@
+package x.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Denis on 12.07.2016.
+ */
+public class MainWindow extends JFrame{
+
+    public MainWindow() throws Exception{
+        UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+        setVisible(true);
+    }
+
+
+
+}
